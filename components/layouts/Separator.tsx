19c7c4c037ae74aa414aa707cd421bc6bf4f180a
ian <im-ian@naver.com>
2025-09-26 09:16:13 +0900

    Add text props for spearator

--- components/layouts/Separator.tsx
+++ components/layouts/Separator.tsx
@@ -4,6 +4,7 @@ import { cn } from "../../lib/utils";
 interface SeparatorProps {
   className?: string;
   size?: Size;
+  text?: string;
 }
 
 const separatorSizes: Record<Size, string> = {
@@ -12,7 +13,24 @@ const separatorSizes: Record<Size, string> = {
   lg: "h-2",
 };
 
-export default function Separator({ className, size = "md" }: SeparatorProps) {
+export default function Separator({
+  className,
+  size = "md",
+  text,
+}: SeparatorProps) {
+  if (text) {
+    return (
+      <div
+        className={cn("relative flex items-center justify-center", className)}
+      >
+        <div className={cn("bg-gray-200 w-full", separatorSizes[size])} />
+        <span className="absolute bg-white px-3 text-sm text-gray-500">
+          {text}
+        </span>
+      </div>
+    );
+  }
+
   return (
     <div
       className={cn("bg-gray-200 w-full", className, separatorSizes[size])}
