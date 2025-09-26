1d9ecbbc8504f5442b483acc80e4ec43e0178b87
ian <im-ian@naver.com>
2025-09-26 09:22:54 +0900

    Fix input text color

--- components/inputs/Input.tsx
+++ components/inputs/Input.tsx
@@ -14,7 +14,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         <input
           ref={ref}
           className={cn(
-            "w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2",
+            "w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 text-black",
             {
               "border-gray-300 focus:ring-blue-500 focus:border-blue-500":
                 !error,
